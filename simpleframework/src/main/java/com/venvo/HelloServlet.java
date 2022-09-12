781377020f0eb739941fff702e230de5d99ef948
venvo <m13434344340@163.com>
2022-09-12 12:11:07 +0800

    log4j

--- simpleframework/src/main/java/com/venvo/HelloServlet.java
+++ simpleframework/src/main/java/com/venvo/HelloServlet.java
@@ -1,5 +1,8 @@
 package com.venvo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,9 +13,12 @@ import java.io.IOException;
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 
+    Logger logger = LoggerFactory.getLogger(HelloServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name="我的简易框架";
+        logger.debug("name is"+name);
         req.setAttribute("name",name);
         req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp);
 
